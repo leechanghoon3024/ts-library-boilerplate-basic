15720ee74bcd2be8c18e0b73ac1e32912e998af2
lchanghoon <lchanghoon12@gmail.com>
2023-10-10 16:06:55 +0900

    add. prettier & test.html

--- build/types/index.d.ts
+++ build/types/index.d.ts
@@ -1,6 +1,7 @@
-import "./index.css";
+import './index.css';
 declare class MyLibrary {
     constructor();
+    test(): void;
     myMethod: () => boolean;
 }
 export default MyLibrary;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,14 +1,17 @@
-import "./index.css";
+import './index.css';
 
 class MyLibrary {
-  constructor() {
-    console.log("Library constructor loaded");
-  }
+    constructor() {
+        console.log('Library constructor loaded');
+    }
+    test() {
+        console.log('dd');
+    }
 
-  myMethod = (): boolean => {
-    console.log("Library method fired");
-    return true;
-  };
+    myMethod = (): boolean => {
+        console.log('Library method fired');
+        return true;
+    };
 }
 
 export default MyLibrary;
